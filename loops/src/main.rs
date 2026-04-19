ec8aae458f53df06e172a5544a4c4028d48963f4
ASpooky <sysk-s0307@docomo.ne.jp>
2026-04-20 01:26:56 +0900

    フィボナッチ数列実装

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -26,4 +26,14 @@ fn main() {
     for i in a {
         println!("{i}");
     }
+
+    println!("fib 12 = {}",fib(12))
+}
+
+fn fib(n:u32)->u32{
+    match n{
+        0 => 0,
+        1 => 1,
+        _ =>fib(n-1)+fib(n-2),
+    }
 }
