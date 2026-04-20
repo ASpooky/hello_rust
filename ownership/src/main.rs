0bbb57b5ef45a36d900954878987c036eafc0776
ASpooky <sysk-s0307@docomo.ne.jp>
2026-04-21 00:09:36 +0900

    feat: ownership int

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    let s = "hello";
+    let mut s1 = String::from("hello");
+    change(&mut s1);
+
+    println!("s1 is {s1}.")
 }
+
+fn change(s: &mut String) {
+    s.push_str(",world");
+}
+
